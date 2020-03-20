7102abd307df0070943d261fe382b360f7fc970e
runny2018 <runny2018@gmail.com>
2020-03-20 11:59:27 +0530

    First Commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Text, StyleSheet, View} from 'react-native'
+
+const ComponentsScreen = () => {
+    const name = "Rohan";
+
+    return (<View>
+            <Text style={styles.textStyle}>Getting started with react native!</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name} </Text>
+        </View>);
+}
+
+const styles=StyleSheet.create({
+    textStyle:{
+        fontSize:45
+    },
+    subHeaderStyle:{
+        fontSize:20
+    }
+})
+
+export default ComponentsScreen
